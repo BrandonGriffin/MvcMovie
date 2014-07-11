9092bdfe9820beb50b2c3b3b50eae99d6d63cd90
Brandon Griffin <bgriffin@qualtrax.com>
2014-07-10 23:17:27 -0400

    Initial commit. Tutorial 9 complete.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public Int32 Id { get; set; }
+        public String Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; }
+        public Decimal Price { get; set; } 
+        public String Rating  { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
